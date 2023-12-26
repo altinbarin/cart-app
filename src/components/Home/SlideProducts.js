29cc1e13eaa7bb0292583507eb85c5b3cebf52a6
altinbarin <furkanaltinbarin@gmail.com>
2023-12-26 20:04:57 +0300

    26 aralık sliderAdded

--- src/components/Home/SlideProducts.js
+++ src/components/Home/SlideProducts.js
@@ -1,68 +1,71 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import SlideProductsCSS from '../../style/SlideProducts.module.css'
+import React, { useState, useEffect, useRef } from 'react';
+import SlideProductsCSS from '../../style/SlideProducts.module.css';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import productsData from '../../products.json';
 
 function SlideProducts() {
-
   const [index, setIndex] = useState(0);
   const [products, setProducts] = useState([]);
+  const progressBarRef = useRef(null);
+  const displayDuration = 3000; // Fotoğrafın ekranda kalma süresi (3 saniye)
 
-   const nextSlide = () => {
-      setIndex((index)=>{
-        let newIndex = index+1;
-        if(newIndex > 4){
-          newIndex= 0;
-        }
-        return newIndex;
-      })
+  useEffect(() => {
+    setProducts(productsData);
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % products.length);
+      resetProgressBar();
+    }, displayDuration);
+
+    return () => {
+      clearInterval(timer);
     };
+  }, [index, products]);
 
-    const previousSlide = () => {
-      setIndex((index)=>{
-        let newIndex = index-1;
-        if(newIndex ===-1){
-          newIndex= 4;
-        }
-        return newIndex;
-      })
-    };
+  const resetProgressBar = () => {
+    clearInterval(progressBarRef.current);
+    progressBarRef.current.style.transition = 'none';
+    progressBarRef.current.style.width = '0%';
+    setTimeout(() => {
+      progressBarRef.current.style.transition = `width ${displayDuration / 1000}s linear`;
+      progressBarRef.current.style.width = '100%';
+    }, 50);
+  };
 
+  const handleNextSlide = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % products.length);
+    resetProgressBar();
+  };
 
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products').then(response=>{
-      return response.json()
-    })
-    .then(data=>{
-      setProducts(data.products)
-      // console.log(products)
-    })
-    .catch(error=>{
-      console.log(error)
-    })
-  },[]);
-
+  const handlePrevSlide = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + products.length) % products.length);
+    resetProgressBar();
+  };
 
   return (
-    <div className='Products'>    
-          {products.length>0 &&(
-             <div className={SlideProductsCSS.card} key={products[index].id}>
-             <div className={SlideProductsCSS.container}>
-             <h1 className={SlideProductsCSS.title}>{products[index].title}</h1>
-             <img className={SlideProductsCSS.productImage} src={products[index].images[0]}/>
-             <p className={SlideProductsCSS.description}>{products[index].description}</p>
-             <h3 className={SlideProductsCSS.discount}>Discount: {products[index].discountPercentage}%</h3>
-             <h2 className={SlideProductsCSS.priceWithoutDiscount}>{Math.ceil((products[index].price)/(1-(products[index].discountPercentage/100)))}$</h2>
-             <h2 className={SlideProductsCSS.price}>{products[index].price}$</h2>
-             <button className={SlideProductsCSS.addToCart}>Add To Cart</button> <br/>
-             <button className={SlideProductsCSS.previousSlideButton} onClick={previousSlide}><ArrowBackIosIcon/></button>
-             <button className={SlideProductsCSS.nextSlideButton} onClick={nextSlide}><ArrowForwardIosIcon/></button>
-             </div>
-             </div>     
-          )}
+    <div className={SlideProductsCSS.slider}>
+      {products.length > 0 && (
+        <div className={SlideProductsCSS.slide}>
+          <ArrowBackIosIcon className={SlideProductsCSS.leftArrow} onClick={handlePrevSlide} />
+          <div className={SlideProductsCSS.active}>
+            {index >= 0 && index < products.length && (
+              <img src={products[index].image} alt={`Product ${index + 1}`} />
+            )}
+            <div className={SlideProductsCSS.progressContainer}>
+              <div
+                className={SlideProductsCSS.progressBar}
+                ref={progressBarRef}
+              />
+            </div>
+          </div>
+          <ArrowForwardIosIcon className={SlideProductsCSS.rightArrow} onClick={handleNextSlide} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default SlideProducts
+export default SlideProducts;
